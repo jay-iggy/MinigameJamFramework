730109b1f14154cd4f24d5944edc25c7449f3970
cu5tin <cu5tin@outlook.com>
2024-11-07 13:57:49 -0500

    only start if all players are connected

--- Assets/Game/MinigameFramework/Menu/MenuScript.cs
+++ Assets/Game/MinigameFramework/Menu/MenuScript.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Game.MinigameFramework.Scripts.Framework.PlayerInfo;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuScript : MonoBehaviour {
+    [SerializeField] Button startButton;
     public void LoadRandomMinigame() {
         MinigameManager.instance.LoadRandomMinigame();
+    }
+
+    public void Update() {
+        startButton.interactable = PlayerManager.AreAllPlayersConnected();
     }
     
     List<PlayerSlot> playerSlots = new List<PlayerSlot>();
