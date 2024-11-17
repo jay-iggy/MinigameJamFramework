6f9015554399c65202e801bc21deb47b8628a276
cu5tin <cu5tin@outlook.com>
2024-11-17 04:35:09 -0500

    FumperFalls: gathering snow mechanic

--- Assets/Game/Examples/FumperFalls/FumperFallsPawn.cs
+++ Assets/Game/Examples/FumperFalls/FumperFallsPawn.cs
@@ -9,6 +9,11 @@ namespace Game.Examples {
     public class FumperFallsPawn : Pawn {
         [SerializeField] private float speed = 8f;
         [SerializeField] private float gravity = -50f;
+        private float snowTotal=0;
+        [SerializeField] private float distanceToSnow = 0.1f;
+        [SerializeField] private AnimationCurve snowSizeCurve;
+        [SerializeField] private AnimationCurve snowMassCurve;
+        [SerializeField] private AnimationCurve snowSpeedCurve;
 
         private Vector2 _moveInput = Vector2.zero;
 
@@ -23,17 +28,40 @@ namespace Game.Examples {
             _rigidbody = GetComponent<Rigidbody>();
         }
         
+        
+        public float distanceTraveled = 0;
         private void Update() {
             // Gravity
             _rigidbody.velocity += gravity * Time.deltaTime * Vector3.up;
             // Movement
             _rigidbody.angularVelocity += new Vector3(_moveInput.y * speed, 0, -_moveInput.x * speed);
+            
+            // if the player's velocity in the same direction of their move input, increase distance traveled
+            float dot = Vector3.Dot(_rigidbody.velocity, _moveInput);
+            if(dot > 0) {
+                distanceTraveled += Vector3.Dot(_rigidbody.velocity, _moveInput) * Time.deltaTime;
+                if(distanceTraveled > 0.1f) {
+                    IncreaseSnow(distanceTraveled * distanceToSnow);
+                    distanceTraveled = 0;
+                }
+            }
         }
 
         // Handle input
         protected override void OnActionPressed(InputAction.CallbackContext context) {
             // Move
             if (context.action.name == "Move") _moveInput = context.ReadValue<Vector2>();
+        }
+        
+        private void IncreaseSnow(float snow) {
+            SetSnowTotal(snowTotal + snow);
+        }
+        private void SetSnowTotal(float snow) {
+            snowTotal = snow;
+            float scale = snowSizeCurve.Evaluate(snowTotal);
+            transform.localScale = new Vector3(scale, scale, scale);
+            _rigidbody.mass = snowMassCurve.Evaluate(snowTotal);
+            speed = snowSpeedCurve.Evaluate(snowTotal);
         }
     }
 }
