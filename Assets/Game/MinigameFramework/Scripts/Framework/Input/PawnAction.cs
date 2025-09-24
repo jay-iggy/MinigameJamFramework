69d616057f9649143c1f735fc3cc2a513d5678cd
cu5tin <cu5tin@outlook.com>
2025-09-24 17:56:08 -0400

    Create PawnAction static class.
    Contains static variables with the names of all possible pawn actions for use as an alternate to raw string text while handling pawn input

---
+++ Assets/Game/MinigameFramework/Scripts/Framework/Input/PawnAction.cs
@@ -0,0 +1,12 @@
+namespace Game.MinigameFramework.Scripts.Framework.Input {
+    public static class PawnAction {
+        public static string Move = "Move";
+        public static string Look = "Look";
+        public static string ButtonA = "ButtonA";
+        public static string ButtonB = "ButtonB";
+        public static string ButtonX = "ButtonX";
+        public static string ButtonY = "ButtonY";
+        public static string ButtonL = "ButtonL";
+        public static string ButtonR = "ButtonR";
+    }
+}
