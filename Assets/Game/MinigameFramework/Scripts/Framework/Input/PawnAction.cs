e132e326b09bf775e2996a0ed610958a20dd3c3d
cu5tin <cu5tin@outlook.com>
2025-09-25 14:16:33 -0400

    Updated PawnAction to remove set permissions. Removed redundant InputName class.

--- Assets/Game/MinigameFramework/Scripts/Framework/Input/PawnAction.cs
+++ Assets/Game/MinigameFramework/Scripts/Framework/Input/PawnAction.cs
@@ -1,12 +1,12 @@
 namespace Game.MinigameFramework.Scripts.Framework.Input {
     public static class PawnAction {
-        public static string Move = "Move";
-        public static string Look = "Look";
-        public static string ButtonA = "ButtonA";
-        public static string ButtonB = "ButtonB";
-        public static string ButtonX = "ButtonX";
-        public static string ButtonY = "ButtonY";
-        public static string ButtonL = "ButtonL";
-        public static string ButtonR = "ButtonR";
+        public static string Move { get; } = "Move";
+        public static string Look { get; } = "Look";
+        public static string ButtonA { get; } = "ButtonA";
+        public static string ButtonB { get; } = "ButtonB";
+        public static string ButtonX { get; } = "ButtonX";
+        public static string ButtonY { get; } = "ButtonY";
+        public static string ButtonL { get; } = "ButtonL";
+        public static string ButtonR { get; } = "ButtonR";
     }
 }
