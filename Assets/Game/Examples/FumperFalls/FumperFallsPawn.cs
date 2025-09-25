22a04ad5df60399bd9b387140fcb167a3524f7a4
cu5tin <cu5tin@outlook.com>
2025-09-25 14:29:15 -0400

    Update FumperFallsPawn.cs

--- Assets/Game/Examples/FumperFalls/FumperFallsPawn.cs
+++ Assets/Game/Examples/FumperFalls/FumperFallsPawn.cs
@@ -7,18 +7,18 @@ using UnityEngine.InputSystem;
 namespace Examples.FumperFalls {
     [RequireComponent(typeof(Rigidbody))]
     public class FumperFallsPawn : Pawn {
+        [Header("Movement")]
         [SerializeField] private float speed = 8f;
         [SerializeField] private float gravity = -50f;
-        private float snowTotal=0;
+        private Vector2 _moveInput = Vector2.zero;
+        private Rigidbody _rigidbody;
+        [Header("Snow Accumulation")]
         [SerializeField] private float distanceToSnow = 0.1f;
         [SerializeField] private AnimationCurve snowSizeCurve;
         [SerializeField] private AnimationCurve snowMassCurve;
         [SerializeField] private AnimationCurve snowSpeedCurve;
-        public float distanceTraveled = 0;
-
-        private Vector2 _moveInput = Vector2.zero;
-
-        private Rigidbody _rigidbody;
+        private float distanceTraveled = 0;
+        private float snowTotal=0;
 
         // Disable Unity's default gravity when this component is added
         private void Reset() {
@@ -42,27 +42,28 @@ namespace Examples.FumperFalls {
             
             UpdateSnowAccumulation();
         }
-        
-        
-        private void UpdateSnowAccumulation() {
-            float dot = Vector3.Dot(_rigidbody.velocity, _moveInput);
-            if(dot > 0) { // if the player's velocity in the same direction of their move input
-                distanceTraveled += Vector3.Dot(_rigidbody.velocity, _moveInput) * Time.deltaTime;
-                if(distanceTraveled > 0.1f) {
-                    IncreaseSnow(distanceTraveled * distanceToSnow);
-                    distanceTraveled = 0;
+
+        #region Snow Accumulation
+            private void UpdateSnowAccumulation() {
+                float dot = Vector3.Dot(_rigidbody.velocity, _moveInput);
+                if(dot > 0) { // if the player's velocity in the same direction of their move input
+                    distanceTraveled += Vector3.Dot(_rigidbody.velocity, _moveInput) * Time.deltaTime;
+                    if(distanceTraveled > 0.1f) {
+                        IncreaseSnow(distanceTraveled * distanceToSnow);
+                        distanceTraveled = 0;
+                    }
                 }
             }
-        }
-        private void IncreaseSnow(float snow) {
-            SetSnowTotal(snowTotal + snow);
-        }
-        private void SetSnowTotal(float snow) {
-            snowTotal = snow;
-            float scale = snowSizeCurve.Evaluate(snowTotal);
-            transform.localScale = new Vector3(scale, scale, scale);
-            _rigidbody.mass = snowMassCurve.Evaluate(snowTotal);
-            speed = snowSpeedCurve.Evaluate(snowTotal);
-        }
+            private void IncreaseSnow(float snow) {
+                SetSnowTotal(snowTotal + snow);
+            }
+            private void SetSnowTotal(float snow) {
+                snowTotal = snow;
+                float scale = snowSizeCurve.Evaluate(snowTotal);
+                transform.localScale = new Vector3(scale, scale, scale);
+                _rigidbody.mass = snowMassCurve.Evaluate(snowTotal);
+                speed = snowSpeedCurve.Evaluate(snowTotal);
+            }
+        #endregion
     }
 }
