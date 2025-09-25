a4eff1f644fddfc28795df16b628aed111a2331e
cu5tin <cu5tin@outlook.com>
2025-09-25 14:24:30 -0400

    Update PawnAction.cs

--- Assets/Game/MinigameFramework/Scripts/Framework/Input/PawnAction.cs
+++ Assets/Game/MinigameFramework/Scripts/Framework/Input/PawnAction.cs
@@ -1,12 +1,12 @@
 namespace Game.MinigameFramework.Scripts.Framework.Input {
     public static class PawnAction {
-        public static string Move { get; } = "Move";
-        public static string Look { get; } = "Look";
-        public static string ButtonA { get; } = "ButtonA";
-        public static string ButtonB { get; } = "ButtonB";
-        public static string ButtonX { get; } = "ButtonX";
-        public static string ButtonY { get; } = "ButtonY";
-        public static string ButtonL { get; } = "ButtonL";
-        public static string ButtonR { get; } = "ButtonR";
+        public const string Move = "Move";
+        public const string Look = "Look";
+        public const string ButtonA = "ButtonA";
+        public const string ButtonB = "ButtonB";
+        public const string ButtonX = "ButtonX";
+        public const string ButtonY = "ButtonY";
+        public const string ButtonL = "ButtonL";
+        public const string ButtonR = "ButtonR";
     }
 }
