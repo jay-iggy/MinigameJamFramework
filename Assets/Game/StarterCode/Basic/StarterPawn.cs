718ae5494c8c6b217c355dfeb20a66f094d16c5a
cu5tin <cu5tin@outlook.com>
2025-10-26 20:11:49 -0400

    Added comments to Starter Pawns

--- Assets/Game/StarterCode/Basic/StarterPawn.cs
+++ Assets/Game/StarterCode/Basic/StarterPawn.cs
@@ -7,6 +7,10 @@ using UnityEngine.InputSystem;
 namespace Starter.Basic {
     public class StarterPawn : Pawn {
         Vector2 _moveInput = Vector2.zero;
+        
+        void Update() {
+            // Implement movement
+        }
 
         protected override void OnActionPressed(InputAction.CallbackContext context) {
             if (context.action.name == PawnAction.Move) {
