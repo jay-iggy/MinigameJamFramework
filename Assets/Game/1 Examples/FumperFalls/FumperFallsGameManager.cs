5800b08df0139dd0ddbdb73c5ba6d2aa9900af65
cu5tin <cu5tin@outlook.com>
2025-11-04 18:16:29 -0500

    Update FumperFallsGameManager.cs

--- Assets/Game/1 Examples/FumperFalls/FumperFallsGameManager.cs
+++ Assets/Game/1 Examples/FumperFalls/FumperFallsGameManager.cs
@@ -46,7 +46,7 @@ namespace Examples.FumperFalls {
             }
             _deaths++; // also count deaths for pawns not bound to a player
 
-            if (_deaths == 3) {
+            if (_deaths >= 3) {
                 StartCoroutine(EndMinigame());
             }
         }
